e8422500975f5ac8fe1ff60b8f0db9c638b45fc2
tlasu <>
2025-05-29 02:51:03 +0900

    ✨ feat: 行政コードの型をstrまたはintに変更し、行政コードが"000"の場合にGeoDataFrameを集約する処理を追加

--- util/get_pref_gdf.py
+++ util/get_pref_gdf.py
@@ -4,20 +4,26 @@ from pathlib import Path
 import geopandas as gpd
 import requests
 
-def get_pref_gdf(gyosei_code:str):
+def get_pref_gdf(gyosei_code:str | int):
     """
     行政コードを指定して、指定した行政地域のgeodataframeを取得する
     gyosei_code: 行政コード
     """
-    assert len(gyosei_code) == 5
-    #08220
+
+    assert len(gyosei_code) == 5, f"行政コードは5桁で指定してください: {gyosei_code}"
+
     pref_code = gyosei_code[:2]
     url = f"https://raw.githubusercontent.com/tlasu/jpn-geo/master/parquet/N03/N03-20240101_{pref_code}.parquet"
     response = requests.get(url).content
-    gdf = gpd.read_parquet(io.BytesIO(response))
-    return gdf[gdf.N03_007 == gyosei_code]
+    if gyosei_code[2:] == "000":
+        gdf = gpd.read_parquet(io.BytesIO(response)).dissolve(by="N03_001")
+        return gdf
+    else:
+        gdf = gpd.read_parquet(io.BytesIO(response))
+        return gdf[gdf.N03_007 == gyosei_code]
 
 if __name__ == "__main__":
-    gyosei_code = "08220"
-    gdf = get_pref_gdf("08220").to_parquet(f"{gyosei_code}.parquet")
-# %%
+    gyosei_code = "08000"
+    gdf = get_pref_gdf(gyosei_code)
+    gdf.to_parquet(f"{gyosei_code}.parquet")
+    gdf.info()
